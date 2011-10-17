f9ffed89b158ca8c6c352ef229e461cc4da5f5e1
bmcquade <bmcquade@google.com>
2011-10-17 23:57:23 +0000

    Make code ANSI C89-friendly.

--- src/domain_registry/private/registry_search.c
+++ src/domain_registry/private/registry_search.c
@@ -1,16 +1,18 @@
-// Copyright 2011 Google Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ * Copyright 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include "domain_registry/domain_registry.h"
 
@@ -20,8 +22,30 @@
 #include "domain_registry/private/string_util.h"
 #include "domain_registry/private/trie_search.h"
 
-// RFCs 1035 and 1123 specify a max hostname length of 255 bytes.
+/* RFCs 1035 and 1123 specify a max hostname length of 255 bytes. */
 static const size_t kMaxHostnameLen = 255;
+
+/* strdup() is not part of ANSI C89 so we define our own. */
+static char* StrDup(const char* s) {
+  const size_t len = strlen(s);
+  char* s2 = malloc(len + 1);
+  if (s2 == NULL) {
+    return NULL;
+  }
+  memcpy(s2, s, len);
+  s2[len] = 0;
+  return s2;
+}
+
+/* strnlen() is not part of ANSI C89 so we define our own. */
+static size_t StrnLen(const char* s, size_t max) {
+  const char* end = s + max;
+  const char* i;
+  for (i = s; i < end; ++i) {
+    if (*i == 0) break;
+  }
+  return i - s;
+}
 
 static int IsStringASCII(const char* s) {
   const char* it = s;
@@ -35,25 +59,29 @@ static int IsStringASCII(const char* s) {
 }
 
 static int IsValidHostname(const char* hostname) {
-  // http://www.ietf.org/rfc/rfc1035.txt (DNS) and
-  // http://tools.ietf.org/html/rfc1123 (Internet host requirements)
-  // specify a maximum hostname length of 255 characters. To make sure
-  // string comparisons, etc are bounded elsewhere in the codebase, we
-  // enforce the 255 character limit here. There are various other
-  // hostname constraints specified in the RFCs (63 bytes per
-  // hostname-part, etc) but we do not enforce those here since doing
-  // so would not change correctness of the overall implementation,
-  // and it's possible that hostnames used in other contexts
-  // (e.g. outside of DNS) would not be subject to the 63-byte
-  // hostname-part limit. So we let the DNS layer enforce its policy,
-  // and enforce only the maximum hostname length here.
-  if (strnlen(hostname, kMaxHostnameLen + 1) > kMaxHostnameLen) {
+  /*
+   * http://www.ietf.org/rfc/rfc1035.txt (DNS) and
+   * http://tools.ietf.org/html/rfc1123 (Internet host requirements)
+   * specify a maximum hostname length of 255 characters. To make sure
+   * string comparisons, etc are bounded elsewhere in the codebase, we
+   * enforce the 255 character limit here. There are various other
+   * hostname constraints specified in the RFCs (63 bytes per
+   * hostname-part, etc) but we do not enforce those here since doing
+   * so would not change correctness of the overall implementation,
+   * and it's possible that hostnames used in other contexts
+   * (e.g. outside of DNS) would not be subject to the 63-byte
+   * hostname-part limit. So we let the DNS layer enforce its policy,
+   * and enforce only the maximum hostname length here.
+   */
+  if (StrnLen(hostname, kMaxHostnameLen + 1) > kMaxHostnameLen) {
     return 0;
   }
 
-  // All hostnames must contain only ASCII characters. If a hostname
-  // is passed in that contains non-ASCII (e.g. an IDN that hasn't been
-  // converted to ASCII via punycode) we want to reject it outright.
+  /*
+   * All hostnames must contain only ASCII characters. If a hostname
+   * is passed in that contains non-ASCII (e.g. an IDN that hasn't been
+   * converted to ASCII via punycode) we want to reject it outright.
+   */
   if (IsStringASCII(hostname) == 0) {
     return 0;
   }
@@ -61,9 +89,11 @@ static int IsValidHostname(const char* hostname) {
   return 1;
 }
 
-// Get a pointer to the beginning of the valid registry. If rule_part
-// is an exception component, this will seek past the
-// rule_part. Otherwise this will simply return the component itself.
+/*
+ * Get a pointer to the beginning of the valid registry. If rule_part
+ * is an exception component, this will seek past the
+ * rule_part. Otherwise this will simply return the component itself.
+ */
 static const char* GetDomainRegistryStr(const char* rule_part,
                                         const char* component) {
   if (IsExceptionComponent(rule_part)) {
@@ -73,11 +103,13 @@ static const char* GetDomainRegistryStr(const char* rule_part,
   }
 }
 
-// Iterates the hostname-parts between start and end in reverse order,
-// separated by the character specified by sep. For instance if the
-// string between start and end is "foo\0bar\0com" and sep is the null
-// character, we will return a pointer to "com", then "bar", then
-// "foo".
+/*
+ * Iterates the hostname-parts between start and end in reverse order,
+ * separated by the character specified by sep. For instance if the
+ * string between start and end is "foo\0bar\0com" and sep is the null
+ * character, we will return a pointer to "com", then "bar", then
+ * "foo".
+ */
 static const char* GetNextHostnamePartImpl(const char* start,
                                            const char* end,
                                            char sep,
@@ -88,9 +120,11 @@ static const char* GetNextHostnamePartImpl(const char* start,
   if (*ctx == NULL) {
     *ctx = (void*) end;
 
-    // Special case: a single trailing dot indicates a fully-qualified
-    // domain name. Skip over it.
-    if (*(end - 1) == 0) {
+    /*
+     * Special case: a single trailing dot indicates a fully-qualified
+     * domain name. Skip over it.
+     */
+     if (*(end - 1) == 0) {
       *ctx = (void*) (end - 1);
     }
   }
@@ -103,10 +137,12 @@ static const char* GetNextHostnamePartImpl(const char* start,
     }
   }
   if (last != start && *start != 0) {
-    // Special case: If we didn't find a match, but the context
-    // indicates that we haven't visited the first component yet, and
-    // there is a non-NULL first component, then visit the first
-    // component.
+    /*
+     * Special case: If we didn't find a match, but the context
+     * indicates that we haven't visited the first component yet, and
+     * there is a non-NULL first component, then visit the first
+     * component.
+     */
     *ctx = (void*) start;
     return start;
   }
@@ -124,8 +160,10 @@ static const char* GetNextHostnamePart(const char* start,
   return hostname_part;
 }
 
-// Iterate over all hostname-parts between value and value_end, where
-// the hostname-parts are separated by character sep.
+/*
+ * Iterate over all hostname-parts between value and value_end, where
+ * the hostname-parts are separated by character sep.
+ */
 static const char* GetRegistryForHostname(const char* value,
                                           const char* value_end,
                                           const char sep) {
@@ -134,8 +172,10 @@ static const char* GetRegistryForHostname(const char* value,
   const char* component = NULL;
   const char* last_valid = NULL;
 
-  // Iterate over the hostname components one at a time, e.g. if value
-  // is foo.com, we will first visit component com, then component foo.
+  /*
+   * Iterate over the hostname components one at a time, e.g. if value
+   * is foo.com, we will first visit component com, then component foo.
+   */
   while ((component =
           GetNextHostnamePart(value, value_end, sep, &ctx)) != NULL) {
     const char* leaf_node;
@@ -151,8 +191,10 @@ static const char* GetRegistryForHostname(const char* value,
       last_valid = NULL;
     }
     if (HasLeafChildren(current)) {
-      // The child nodes are in the leaf node table, so perform a
-      // search in that table.
+      /*
+       * The child nodes are in the leaf node table, so perform a
+       * search in that table.
+       */
       component = GetNextHostnamePart(value, value_end, sep, &ctx);
       if (component == NULL) {
         break;
@@ -177,21 +219,25 @@ static size_t GetRegistryLengthImpl(
   size_t match_len;
 
   while (*value == sep && value < value_end) {
-    // Skip over leading separators.
+    /* Skip over leading separators. */
     ++value;
   }
   registry = GetRegistryForHostname(value, value_end, sep);
   if (registry == NULL) {
-    // Didn't find a match. If unknown registries are allowed, see if
-    // the root hostname part is not in the table. If so, consider it to be a
-    // valid registry, and return its length.
+    /*
+     * Didn't find a match. If unknown registries are allowed, see if
+     * the root hostname part is not in the table. If so, consider it to be a
+     * valid registry, and return its length.
+     */
     if (allow_unknown_registries != 0) {
       void* ctx = NULL;
       const char* root_hostname_part =
           GetNextHostnamePart(value, value_end, sep, &ctx);
-      // See if the root hostname-part is in the table. If it's not in
-      // the table, then consider the unknown registry to be a valid
-      // registry.
+      /*
+       * See if the root hostname-part is in the table. If it's not in
+       * the table, then consider the unknown registry to be a valid
+       * registry.
+       */
       if (root_hostname_part != NULL &&
           FindRegistryNode(root_hostname_part, NULL) == NULL) {
         registry = root_hostname_part;
@@ -202,7 +248,7 @@ static size_t GetRegistryLengthImpl(
     }
   }
   if (registry < value || registry >= value_end) {
-    // Error cases.
+    /* Error cases. */
     DCHECK(registry >= value);
     DCHECK(registry < value_end);
     return 0;
@@ -213,22 +259,28 @@ static size_t GetRegistryLengthImpl(
 
 size_t GetRegistryLength(const char* hostname) {
   const char* buf_end;
+  char* buf;
   size_t registry_length;
 
   if (IsValidHostname(hostname) == 0) {
     return 0;
   }
 
-  // Replace dots between hostname parts with the null byte. This
-  // allows us to index directly into the string and refer to each
-  // hostname-part as if it were its own null-terminated string.
-  char* buf = strdup(hostname);
+  /*
+   * Replace dots between hostname parts with the null byte. This
+   * allows us to index directly into the string and refer to each
+   * hostname-part as if it were its own null-terminated string.
+   */
+  buf = StrDup(hostname);
+  if (buf == NULL) {
+    return 0;
+  }
   ReplaceChar(buf, '.', '\0');
 
   buf_end = buf + strlen(hostname);
   DCHECK(*buf_end == 0);
 
-  // Normalize the input by converting all characters to lowercase.
+  /* Normalize the input by converting all characters to lowercase. */
   ToLowerASCII(buf, buf_end);
   registry_length = GetRegistryLengthImpl(buf, buf_end, '\0', 0);
   free(buf);
@@ -237,22 +289,28 @@ size_t GetRegistryLength(const char* hostname) {
 
 size_t GetRegistryLengthAllowUnknownRegistries(const char* hostname) {
   const char* buf_end;
+  char* buf;
   size_t registry_length;
 
   if (IsValidHostname(hostname) == 0) {
     return 0;
   }
 
-  // Replace dots between hostname parts with the null byte. This
-  // allows us to index directly into the string and refer to each
-  // hostname-part as if it were its own null-terminated string.
-  char* buf = strdup(hostname);
+  /*
+   * Replace dots between hostname parts with the null byte. This
+   * allows us to index directly into the string and refer to each
+   * hostname-part as if it were its own null-terminated string.
+   */
+  buf = StrDup(hostname);
+  if (buf == NULL) {
+    return 0;
+  }
   ReplaceChar(buf, '.', '\0');
 
   buf_end = buf + strlen(hostname);
   DCHECK(*buf_end == 0);
 
-  // Normalize the input by converting all characters to lowercase.
+  /* Normalize the input by converting all characters to lowercase. */
   ToLowerASCII(buf, buf_end);
   registry_length = GetRegistryLengthImpl(buf, buf_end, '\0', 1);
   free(buf);
--- src/domain_registry/private/string_util.h
+++ src/domain_registry/private/string_util.h
@@ -1,16 +1,18 @@
-// Copyright 2011 Google Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ * Copyright 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #ifndef DOMAIN_REGISTRY_PRIVATE_STRING_UTIL_H_
 #define DOMAIN_REGISTRY_PRIVATE_STRING_UTIL_H_
@@ -23,24 +25,24 @@
 static const char kUpperLowerDistance = 'A' - 'a';
 
 #if _WINDOWS
-#define inline __inline
+#define __inline__ __inline
 #endif
 
-static inline int IsWildcardComponent(const char* component) {
+static __inline__ int IsWildcardComponent(const char* component) {
   if (component[0] == '*') {
     return 1;
   }
   return 0;
 }
 
-static inline int IsExceptionComponent(const char* component) {
+static __inline__ int IsExceptionComponent(const char* component) {
   if (component[0] == '!') {
     return 1;
   }
   return 0;
 }
 
-static inline int IsInvalidComponent(const char* component) {
+static __inline__ int IsInvalidComponent(const char* component) {
   if (component == NULL ||
       component[0] == 0 ||
       IsExceptionComponent(component) ||
@@ -50,14 +52,14 @@ static inline int IsInvalidComponent(const char* component) {
   return 0;
 }
 
-static inline void ReplaceChar(char* value, char old, char newval) {
+static __inline__ void ReplaceChar(char* value, char old, char newval) {
   while ((value = strchr(value, old)) != NULL) {
     *value = newval;
     ++value;
   }
 }
 
-static inline void ToLowerASCII(char* buf, const char* end) {
+static __inline__ void ToLowerASCII(char* buf, const char* end) {
   for (; buf < end; ++buf) {
     char c = *buf;
     if (c >= 'A' && c <= 'Z') {
@@ -66,18 +68,22 @@ static inline void ToLowerASCII(char* buf, const char* end) {
   }
 }
 
-static inline int HostnamePartCmp(const char *a, const char *b) {
-  // Optimization: do not invoke strcmp() unless the first characters
-  // in each string match. Since we are performing a binary search, we
-  // expect most invocations to strcmp to not have matching arguments,
-  // and thus not invoke strcmp. This reduces overall runtime by 5-10%
-  // on a Linux laptop running a -O2 optimized build.
+static __inline__ int HostnamePartCmp(const char *a, const char *b) {
+  /*
+   * Optimization: do not invoke strcmp() unless the first characters
+   * in each string match. Since we are performing a binary search, we
+   * expect most invocations to strcmp to not have matching arguments,
+   * and thus not invoke strcmp. This reduces overall runtime by 5-10%
+   * on a Linux laptop running a -O2 optimized build.
+   */
   int ret = *(unsigned char *)a - *(unsigned char *)b;
-  // NOTE: we could invoke strcmp on a+1,b+1 if we are
-  // certain that neither a nor b are the empty string. For now we
-  // take the more conservative approach.
+  /*
+   * NOTE: we could invoke strcmp on a+1,b+1 if we are
+   * certain that neither a nor b are the empty string. For now we
+   * take the more conservative approach.
+   */
   if (ret == 0) return strcmp(a, b);
   return ret;
 }
 
-#endif  // DOMAIN_REGISTRY_PRIVATE_STRING_UTIL_H_
+#endif  /* DOMAIN_REGISTRY_PRIVATE_STRING_UTIL_H_ */
